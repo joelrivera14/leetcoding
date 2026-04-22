8f6318bff3bf8faf09b249a87ce92fbde5a2fe3f
joel rivera <107011173+joelrivera14@users.noreply.github.com>
2026-04-22 08:51:08 -0400

    Time: 1 ms (16.34%) | Memory: 15.3 MB (17.26%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -14,28 +14,29 @@ public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         if(root == nullptr)return vector<vector<int>>{};
         vector<vector<int>>ans;
-        deque<TreeNode*> tree;
+        deque<TreeNode*>tree;
         tree.push_back(root);
         tree.push_back(nullptr);
-        deque<int>lvlList;
-        bool isOrderLeft = true;
+        deque<int>levels;
+        bool goingLeft = true;
+        
         while(!tree.empty()){
             TreeNode* node = tree.front();
             tree.pop_front();
             if(node != nullptr){
-                if(isOrderLeft){
-                    lvlList.push_back(node->val);
+                if(goingLeft){
+                    levels.push_back(node->val);
                 }else{
-                lvlList.push_front(node->val);
-            }    
-            if(node->left)tree.push_back(node->left);
-            if(node->right)tree.push_back(node->right);
+                    levels.push_front(node->val);
+                }
+                if(node->left)tree.push_back(node->left);
+                if(node->right)tree.push_back(node->right);
             }else{
-                ans.push_back(vector<int>{lvlList.begin(), lvlList.end()});
-                lvlList.clear();
+                ans.push_back(vector<int>{levels.begin(), levels.end()});
+                levels.clear();
                 if(!tree.empty())tree.push_back(nullptr);
-                isOrderLeft = !isOrderLeft;
-            }   
+                goingLeft=!goingLeft;
+            }
         }
         return ans;
     }
